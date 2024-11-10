609b867d99606b61c9cb86621dd13d1084d40dac
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-11 00:09:21 +0530

    Initialize my job app

---
+++ src/main/java/com/example/TeluskoJobRest/TeluskoJobRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeluskoJobRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoJobRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoJobRestApplication.class, args);
+	}
+
+}
