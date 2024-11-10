df675751baad05234e01408aa2fcc94d73197f0e
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-11 00:01:10 +0530

    Add jobpost class for data

---
+++ src/main/java/com/example/TeluskoJobRest/model/JobPost.java
@@ -0,0 +1,24 @@
+package com.example.TeluskoJobRest.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class JobPost {
+    @Id
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private int reqExperience;
+    private List<String> postTechStack;
+}
