7d2b3f4b89a1f1355aeafaa9af7fbcd7a6a463b8
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-11 12:31:48 +0530

    Added before advice and join point for methods of service

--- src/main/java/com/example/TeluskoJobRest/AOP/LoggingAspect.java
+++ src/main/java/com/example/TeluskoJobRest/AOP/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.example.TeluskoJobRest.AOP;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,9 +14,11 @@ public class LoggingAspect {
 
     private static final Logger LOGGER=LoggerFactory.getLogger(LoggingAspect.class);
 
+    //return type , class name , method name , args
 
-    public void logMethodCall(){
-        LOGGER.info("method called");
+    @Before("execution(* com.example.TeluskoJobRest.Services.*.getJob(..)) || execution(* com.example.TeluskoJobRest.Services.*.load(..))")
+    public void logMethodCall(JoinPoint jp){
+        LOGGER.info("method called  {}", jp.getSignature().getName());
 
     }
 }
