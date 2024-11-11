f2f6d0fd981fc59b9cf2a2f2d3d1f1f0bcee0c00
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-11 12:50:20 +0530

    Add after advices for jobservices method call

--- src/main/java/com/example/TeluskoJobRest/AOP/LoggingAspect.java
+++ src/main/java/com/example/TeluskoJobRest/AOP/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.example.TeluskoJobRest.AOP;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,10 +14,34 @@ public class LoggingAspect {
     private static final Logger LOGGER=LoggerFactory.getLogger(LoggingAspect.class);
 
     //return type , class name , method name , args
+    //com.example.TeluskoJobRest.Services.*.getJob this is point cut
 
     @Before("execution(* com.example.TeluskoJobRest.Services.*.getJob(..)) || execution(* com.example.TeluskoJobRest.Services.*.load(..))")
     public void logMethodCall(JoinPoint jp){
         LOGGER.info("method called  {}", jp.getSignature().getName());
+
+    }
+
+    //run at last
+    //it is like finaly it happens last
+
+    @After("execution(* com.example.TeluskoJobRest.Services.*.getJob(..)) || execution(* com.example.TeluskoJobRest.Services.*.load(..))")
+    public void logMethodCallAfter(JoinPoint jp){
+        LOGGER.info("method Executed  {}", jp.getSignature().getName());
+
+    }
+
+    //call if and only if there will be exception
+
+    @AfterThrowing("execution(* com.example.TeluskoJobRest.Services.*.getJob(..)) || execution(* com.example.TeluskoJobRest.Services.*.load(..))")
+    public void logMethodCallAfterCrash(JoinPoint jp){
+        LOGGER.info("{} method has some issue", jp.getSignature().getName());
+
+    }
+    //call if method compiled successfully
+    @AfterReturning("execution(* com.example.TeluskoJobRest.Services.*.getJob(..)) || execution(* com.example.TeluskoJobRest.Services.*.load(..))")
+    public void logMethodCallAfterSuccess(JoinPoint jp){
+        LOGGER.info("method {} has run successfully", jp.getSignature().getName());
 
     }
 }
